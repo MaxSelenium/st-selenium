3c2844bf9dc926a466246e9ca14a8de8faf403dd
VSK\bokov <bmax@mail.ru>
2018-02-06 23:55:03 +0300

    Первое задание

---
+++ FirstSeleniumTest/TestClass.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace FirstSeleniumTest
+{
+    [TestFixture]
+    public class TestClass
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "http://www.yandex.ru/";
+            driver.FindElement(By.Id("text")).SendKeys("работает");
+            driver.FindElement(By.ClassName("search2__button")).Click();
+            wait.Until(ExpectedConditions.TitleContains("работает — Яндекс:"));
+        }
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
