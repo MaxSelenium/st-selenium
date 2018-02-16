4cca6656816a977d50f8945d02f0154ddbf672ec
Максим <bmax@mail.ru>
2018-02-16 23:17:32 +0300

    Задание 7. Сделайте сценарий, проходящий по всем разделам админки

--- FirstSeleniumTest/TestClass.cs
+++ FirstSeleniumTest/TestClass.cs
@@ -21,19 +21,19 @@ namespace FirstSeleniumTest
         [SetUp]
         public void Start()
         {
-            //driver = new ChromeDriver();
+            driver = new ChromeDriver();
             //driver = new EdgeDriver();
 
-            FirefoxOptions options = new FirefoxOptions();
+            //FirefoxOptions options = new FirefoxOptions();
             //options.BrowserExecutableLocation = @"C:\FF45\firefox.exe";
-            options.UseLegacyImplementation = false;
+            //options.UseLegacyImplementation = true;
 
             //52.6.0
             //options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
-            options.BrowserExecutableLocation = @"C:\Program Files\Firefox Nightly\firefox.exe";
+            //options.BrowserExecutableLocation = @"C:\Program Files\Firefox Nightly\firefox.exe";
             //options.BrowserExecutableLocation = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
-            driver = new FirefoxDriver(options);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            //driver = new FirefoxDriver(options);
+            //wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
             //driver = new FirefoxDriver(options);
             //wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
         }
@@ -56,6 +56,73 @@ namespace FirstSeleniumTest
             driver.FindElement(By.Name("login")).Click();
             wait.Until(ExpectedConditions.TitleIs("My Store"));
         }
+
+        [Test]
+        public void AdminPart()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+
+            int countItemLevel1 = driver.FindElements(By.CssSelector("div#box-apps-menu-wrapper li#app-")).Count();
+            for (int i = 0; i < countItemLevel1; i++)
+            {
+                driver.FindElements(By.CssSelector("div#box-apps-menu-wrapper li#app-"))[i].Click();
+                
+               
+                if  (isExists(By.CssSelector("li#app-[class='selected'] ul.docs")))
+                {
+                    Compare(driver.FindElement(By.CssSelector("h1")).Text, driver.FindElement(By.CssSelector("li#app- li.selected")).Text);
+                    int countItemLevel2 = driver.FindElements(By.CssSelector("li#app-[class='selected'] ul.docs [href ^='http://lo']")).Count();
+                    for (int n = 0; n < countItemLevel2; n++)
+                    {
+                        driver.FindElements(By.CssSelector("li#app-[class='selected'] ul.docs [href ^='http://lo']"))[n].Click();
+
+                        Compare(driver.FindElement(By.CssSelector("h1")).Text,driver.FindElement(By.CssSelector("li#app- li.selected")).Text);
+                    }
+                }
+                else
+                    Compare(driver.FindElement(By.CssSelector("h1")).Text, driver.FindElement(By.CssSelector("li#app-[class='selected']")).Text);
+            }
+            
+        }
+
+        [Test]
+        public void Task7()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+
+            int countItemLevel1 = driver.FindElements(By.CssSelector("div#box-apps-menu-wrapper li#app-")).Count();
+            for (int i = 0; i < countItemLevel1; i++)
+            {
+                driver.FindElements(By.CssSelector("div#box-apps-menu-wrapper li#app-"))[i].Click();
+                Assert.True(isExists(By.CssSelector("h1")));
+                if (isExists(By.CssSelector("li#app-[class='selected'] ul.docs")))
+                {
+                    
+                    int countItemLevel2 = driver.FindElements(By.CssSelector("li#app-[class='selected'] ul.docs [href ^='http://lo']")).Count();
+                    for (int n = 0; n < countItemLevel2; n++)
+                    {
+                        driver.FindElements(By.CssSelector("li#app-[class='selected'] ul.docs [href ^='http://lo']"))[n].Click();
+                        Assert.True(isExists(By.CssSelector("h1")));
+                    }
+                }
+            }
+        }
+        private void Compare (string text1, string text2)
+        {
+            if (text1 != text2)
+                Assert.Fail($"Страница {text1} не равна меню {text2} !");
+        }
+        private bool isExists( By locator  )
+        {
+            return driver.FindElements(locator).Count > 0;
+        }
+
         [TearDown]
         public void stop()
         {
---
+++ Run.bat
@@ -0,0 +1,2 @@
+"C:\Users\Максим\Documents\GitHub\st-selenium\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" "C:\Users\Максим\Documents\GitHub\st-selenium\FirstSeleniumTest\bin\Debug\FirstSeleniumTest.dll"
+pause
